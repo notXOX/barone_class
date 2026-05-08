d0e2b11329bf4489a609253a4f9c8d889b06765b
samuel <samuelariascusano@gmail.com>
2026-05-08 12:06:23 -0300

    Finalizacion de la tarea

--- linux/tarea1.sh
+++ linux/tarea1.sh
@@ -3,18 +3,68 @@
 archive=$(zenity --file-selection)
 echo "Seleccionaste: $archive"
 
+# Validar si canceló la operación
+[ -z "$archive" ] && exit
+
+# Validar si el archivo existe
+if [ ! -f "$archive" ]; then
+    zenity --error --text="El archivo no existe"
+    exit
+fi
+
+# Funciones del script
 reemplazar_palabra() {
-    old=$(zenity --entry --title="Palabra a modificar" --text="Escriba la palabra a modificar")
-    new=$(zenity --entry --title="Palabra nueva" --text="Escriba la palabra nueva")
+    old=$(zenity --entry --title="Palabra a modificar" \ 
+    --text="Escriba la palabra a modificar")
+
+    [ -z "$old" ] && return # Significa que si la variable esta vacía, retorne al inicio
+
+    new=$(zenity --entry --title="Palabra nueva" \
+    --text="Escriba la palabra nueva")
+
+    [ -z "$new" ] && return
 
     nuevo="${archive%.*}_modificado.txt"
     sed "s/$old/$new/g" "$archive" > "$nuevo"
+
+    zenity --info --text="Archivo guardado como:\n$nuevo"
+}
+
+eliminar_lineas() {
+    word=$(zenity --entry --title="Eliminar lineas que contengan una palabra" \
+    --text="Ingrese la palabra")
+
+    [ -z "$word" ] && return
+    
+    nuevo="${archive%.*}_modificado.txt"
+    sed "/$word/d" "$archive" > "$nuevo"
+
+    zenity --info --text="Archivo guardado como:\n$nuevo"
+}
+
+convertir_texto() {
+    op=$(zenity --list --title="Menu de conversion de texto" \
+    --colum="Opciones" \
+    "Mayusculas" \
+    "Minusculas")
+
+    [ -z "$op" ] && return
+
+    nuevo="${archive%.*}_modificado.txt"
+
+    if [ "$op" = Mayusculas ]; then
+        tr '[:lower:]' '[:upper:]' < "$archive" > "$nuevo"
+    else 
+        tr '[:upper:]' '[:lower:]' < "$archive" > "$nuevo"
+    fi
+
+    zenity --info --text="Archivo guardado como:\n$nuevo"
 }
 
 while true; do
     option=$(zenity --list --title="Menu de modificacion de archivos" --text="Seleccione el tipo de modificacion" \
     --column="Opciones" --column="Descripcion" \
-    "1" "Remplazar una palabra por otra" \
+    "1" "Reemplazar una palabra por otra" \
     "2" "Eliminar todas las lineas que contengan una palabra" \
     "3" "Convertir todo a minusculas o mayusculas" \
     "4" "Salir" \
@@ -25,10 +75,10 @@ while true; do
         reemplazar_palabra
     ;;
     "2")
-
+        eliminar_lineas
     ;;
     "3")
-
+        convertir_texto
     ;;
     "4")
         exit
