f520e5c2e5518a1abfff3b2a561b45b87e7c4c44
samuel <samuelariascusano@gmail.com>
2026-05-14 12:13:25 -0300

    errores corregidos

--- linux_barone/index.py
+++ linux_barone/index.py
@@ -1,12 +1,12 @@
 temperatura = int(input("Cuantos grados hace?\n"))
 
-if temperatura < 10:
+if temperatura <= 10:
     print("Está bastante frio")
-elif temperatura > 10 and temperatura < 15:
+elif temperatura > 10 and temperatura <= 15:
     print("Está fresco")
-elif temperatura > 15 and temperatura < 20:
+elif temperatura > 15 and temperatura <= 20:
     print("Está templado")
-elif temperatura > 20 and temperatura < 27:
+elif temperatura > 20 and temperatura <= 27:
     print("Esta caluroso")
 else:
     print("Hace mucho calor")
